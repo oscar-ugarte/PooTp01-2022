72176441d992f12b7cbbf91f769d2789fda664f0
oscar ugarte <oscar.ugrt@gmail.com>
2022-08-11 13:49:01 -0300

    Se agregó un menu de opciones a la clase Test.java para facilitar las pruebas.

--- src/ar/edu/unju/fi/ejercicio1/Test.java
+++ src/ar/edu/unju/fi/ejercicio1/Test.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi.ejercicio1;
 
+import java.util.Scanner;
+
 /*
  * @author Oscar Ugarte
  * @since 1.0
@@ -7,14 +9,52 @@ package ar.edu.unju.fi.ejercicio1;
  * */
 public class Test {
 
+	/**
+	 * Muestra un menu de opciones para facilitar las pruebas.
+	 */
+	public static void mostrarMenu()
+	{
+		System.out.println("*****M E N U*****");
+		System.out.println("0) Salir.");
+		System.out.println("1) Mostrar Vector.");
+		System.out.println("2) Calcular cuadrado de los multiplos de 3.");
+		System.out.println("3) Eliminar un numero del vector.");
+		System.out.println("4) ");
+		System.out.print("Ingrese una opcion: " );
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Integer [] miVector = VectorDeNumeros.crearVector() ;
 		miVector = VectorDeNumeros.cargarVector(miVector);
+		Integer opcion = 5, numero = 0;
+		Scanner teclado = new Scanner(System.in);
 		VectorDeNumeros.mostrarVector(miVector);
-		miVector = VectorDeNumeros.cambiarMultiplosDeTres(miVector);
-		System.out.println();
-		VectorDeNumeros.mostrarVector(miVector);
+		while(opcion != 0)
+		{
+			mostrarMenu();
+			opcion = teclado.nextInt();
+			switch(opcion)
+			{
+				case 0:
+					System.out.println("Saliendo del programa.");
+					break;
+				case 1:
+					VectorDeNumeros.mostrarVector(miVector);
+					break;
+				case 2:
+					miVector = VectorDeNumeros.cambiarMultiplosDeTres(miVector);
+					VectorDeNumeros.mostrarVector(miVector);
+					break;
+				case 3:
+					
+					break;
+				case 4:
+					
+					break;
+				default:
+					System.out.println("Opcion invalida.");
+			}
+		}
 	}
 
 }
